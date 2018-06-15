94ab49a3b8a17c1beae5f443da2cb0c1754c3ce9
Thomas Genin <xt6.thomas.genin@gmail.com>
2018-06-14 23:49:30 -0700

    blessed test

--- generator.js
+++ generator.js
@@ -1,13 +1,13 @@
-const randomWords = require('random-words');
-const fs = require('fs');
+const randomWords = require("random-words");
+const fs = require("fs");
 
 function write() {
-    const word = randomWords();
-    fs.appendFile('/tmp/words.log',  word + "\n", function (err) {
-        if (err) throw err;
-        console.log('Wrote', word);
-    });
-    setTimeout(write, 750);
+  const word = randomWords();
+  fs.appendFile("/tmp/words.log", word + "\n", function(err) {
+    if (err) throw err;
+    console.log("Wrote", word);
+  });
+  setTimeout(write, (Math.random() * 1000).toFixed(3));
 }
 
 write();
--- index.js
+++ index.js
@@ -1,14 +1,86 @@
-const TailLib = require('tail').Tail;
 const config = require("./config.json");
+const TailLib = require("tail").Tail;
+// const clearScreen = require("clear");
+// const clui = require("clui");
+// const chalk = require("chalk");
+
+const blessed = require("blessed");
+const contrib = require("blessed-contrib");
+const screen = blessed.screen();
+
+const grid = new contrib.grid({ rows: 1, cols: 2, screen: screen });
+
+// screen.append(line); //must append before setting data
+// line.setData([data]);
+const rollingLog = grid.set(0, 0, 1, 1, contrib.log, {
+  // fg: "green",
+  // selectedFg: "green",
+  label: "Raw Log"
+});
+
+const table = grid.set(0, 1, 1, 1, contrib.table, {
+  keys: true,
+  fg: "white",
+  selectedFg: "white",
+  selectedBg: "blue",
+  interactive: false,
+  // label: "Active Processes",
+  width: "80%",
+  height: "30%",
+  border: { type: "line", fg: "cyan" },
+  columnSpacing: 10,
+  columnWidth: [7, 12, 15]
+});
 
 const tail = new TailLib(config.source);
 
 let numberOfLines = 0;
+const regExp = new RegExp(config.screens[0].match, "g");
+let tableMatch = {};
 tail.on("line", function(data) {
-    numberOfLines++;
-    console.log(data);
+  numberOfLines++;
+  rollingLog.log(data);
+  const ret = regExp.exec(data);
+
+  if (ret) {
+    const token = ret[0];
+    if (!tableMatch[token]) {
+      tableMatch[token] = 0;
+    }
+    tableMatch[token]++;
+    updateTableData();
+  }
+});
+function updateTableData() {
+  let total = 0;
+  Object.keys(tableMatch).forEach(key => {
+    total += tableMatch[key];
+  });
+  const keys = Object.keys(tableMatch);
+  keys.sort();
+  table.setData({
+    headers: ["What", "count", "%"],
+    data: keys.map(key => {
+      return [
+        key,
+        tableMatch[key],
+        Math.floor((tableMatch[key] / total) * 100)
+      ];
+    })
+  });
+}
+tail.on("error", function(error) {
+  console.log("ERROR: ", error);
 });
 
-tail.on("error", function(error) {
-    console.log('ERROR: ', error);
+screen.key(["escape", "q", "C-c"], function(ch, key) {
+  return process.exit(0);
 });
+
+screen.key(["r"], function(ch, key) {
+  numberOfLines = 0;
+  tableMatch = {};
+  updateTableData();
+});
+
+screen.render();
