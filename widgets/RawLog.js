b2131bf294d3c8540a7f2c78046e1768449f8f31
Thomas Genin <xt6.thomas.genin@gmail.com>
2018-06-21 21:08:07 -0700

    Ability to change the color of a log line if it contains a token

--- widgets/RawLog.js
+++ widgets/RawLog.js
@@ -1,16 +1,56 @@
+const _ = require("lodash");
 const ConfigChecks = require("../utils/configChecks");
 const contrib = require("blessed-contrib");
 
+const VALID_COLORS = ["red", "yellow"];
+
 class RawLog {
   static sanitizeConfig(config) {
-    return Object.assign(ConfigChecks.global(config), {
+    const cleanConfig = Object.assign(ConfigChecks.global(config), {
       prefixTrimIndex: ConfigChecks.sanitizeInteger(
         config,
         "prefixTrimIndex",
         0
       )
     });
+    cleanConfig.highlightLines = [];
+    const configHightLightLines = _.get(config, "highlights.lines", []);
+    if (!_.isArray(configHightLightLines)) {
+      throw new Error(
+        `Widget ${config.name} attribute highlights.lines must be an array`
+      );
+    }
+    configHightLightLines.forEach((lineConfig, idx) => {
+      const color = _.get(lineConfig, "color", null);
+      const token = _.get(lineConfig, "match", null);
+      if (!_.isString(color)) {
+        throw new Error(
+          `Widget ${
+            config.name
+          } attribute highlights.lines[${idx}].color must be a string`
+        );
+      }
+      if (!VALID_COLORS.includes(color)) {
+        throw new Error(
+          `Widget ${
+            config.name
+          } attribute highlights.lines[${idx}].color = "${color}" is invalid. Valid colors are: ${VALID_COLORS.join(
+            ","
+          )}`
+        );
+      }
+      if (!_.isString(token)) {
+        throw new Error(
+          `Widget ${
+            config.name
+          } attribute highlights.lines[${idx}].match must be a string`
+        );
+      }
+      cleanConfig.highlightLines.push({ color, token });
+    });
+    return cleanConfig;
   }
+
   constructor(grid, config) {
     this.rollingLog = grid.set(
       config.row,
@@ -21,16 +61,29 @@ class RawLog {
       {
         width: "90%",
         height: "100%",
-        // fg: "green",
-        // selectedFg: "green",
+        tags: true,
+        style: {
+          scrollbar: {
+            bg: "red",
+            fg: "blue"
+          }
+        },
+        scrollbar: true,
         label: config.name
       }
     );
-    this.prefixTrimIndex = config.prefixTrimIndex;
+    this.config = config;
   }
 
   newLine(line) {
-    line = line.substr(this.prefixTrimIndex);
+    line = line.substr(this.config.prefixTrimIndex);
+    const foundLineMatch = this.config.highlightLines.find(config => {
+      return line.indexOf(config.token) !== -1;
+    });
+    if (foundLineMatch !== undefined) {
+      const color = foundLineMatch.color;
+      line = `{${color}-fg}${line}{/${color}-fg}`;
+    }
     this.rollingLog.log(line);
   }
   reset() {
